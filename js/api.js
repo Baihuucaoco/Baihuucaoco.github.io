fe6355a3fe42d963927ecf65a43be98bd5fcf3be
Baihuucaoco <155862492+Baihuucaoco@users.noreply.github.com>
2024-07-14 12:23:15 +0700

    spck

--- js/api.js
+++ js/api.js
@@ -2,15 +2,45 @@ const url = 'https://chinese-food-db.p.rapidapi.com/';
 const options = {
 	method: 'GET',
 	headers: {
-		'x-rapidapi-key': 'ea1c44a32emsh6d3db03643ce408p1a1ba7jsn4e792869531c',
+		'x-rapidapi-key': '93d16bfe53msh436049637a3c9dbp17f23bjsn5e2bba5a4180',
 		'x-rapidapi-host': 'chinese-food-db.p.rapidapi.com'
 	}
 };
-
-try {
+async function getData(){
+	try {
 	const response = await fetch(url, options);
 	const result = await response.text();
-	console.log(result);
+	var data = JSON.parse(result)
+	console.log(data);
+	for (item of data) {
+		//create it
+		let card = document.createElement("div")
+		//name it
+	card.classList.add("card")
+	//img
+		let imgContainer = document.createElement("div")
+	imgContainer.classList.add("img-container")
+	//khai bao img
+	let image = document.createElement("img")
+		image.setAttribute("src", item.image)
+		imgContainer.appendChild(image)
+		card.appendChild(imgContainer)
+		
+	//items
+		let container = document.createElement("div")
+		container.classList.add("container")
+		let name = document.createElement("h5")
+		name.classList.add("name")
+		name.innerText = item.title
+		container.appendChild(name)
+		let btn = document.createElement("button")
+		btn.innerHTML = "Go to recipe -->"
+		container.appendChild(btn)
+		card.appendChild(container)
+		document.getElementById("product").appendChild(card)
+	}
 } catch (error) {
 	console.error(error);
 }
+}
+getData()
