ba0cefebcd3c1478d9dd113ef36cadf63d51dc7e
Baihuucaoco <155862492+Baihuucaoco@users.noreply.github.com>
2024-08-04 09:57:55 +0700

    Baihuucaoco.github.io

--- login n signup/class/firebase.js
+++ login n signup/class/firebase.js
@@ -1,33 +1,38 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
-  import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut  } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-  
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-  export class Firebase{
-    constructor(){
-  // Your web app's Firebase configuration
-  this.firebaseConfig = {
-    apiKey: "AIzaSyDEfH-BHBzXLtPzF2xxo-g09e4215JN250",
-    authDomain: "spck-ba182.firebaseapp.com",
-    projectId: "spck-ba182",
-    storageBucket: "spck-ba182.appspot.com",
-    messagingSenderId: "330748459048",
-    appId: "1:330748459048:web:c10bc934d5382e5dcb9d1d"
-  };
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+export class Firebase {
+  constructor() {
+    // Your web app's Firebase configuration
+    this.firebaseConfig = {
+      apiKey: "AIzaSyCjx-EKiluf6KJgVlCn1RGEOfAWjIloIbQ",
+      authDomain: "webcuabao.firebaseapp.com",
+      projectId: "webcuabao",
+      storageBucket: "webcuabao.appspot.com",
+      messagingSenderId: "1007186685456",
+      appId: "1:1007186685456:web:99515589a0bffd6c8fa495",
+      measurementId: "G-T4CPEWJ5B0",
+    };
 
-  // Initialize Firebase
-  this.app = initializeApp(this.firebaseConfig);
-  this.auth = getAuth(this.app)
-}
-async signup(email, password){
-  return await createUserWithEmailAndPassword(this, auth, email, password)
-}
-async login(email, password){
-  return await signInWithEmailAndPassword(auth, email, password)
-}
-async logout(){
-  return await signOut(this.auth)
-}
+    // Initialize Firebase
+    this.app = initializeApp(this.firebaseConfig);
+    this.auth = getAuth(this.app);
+  }
+  async signup(email, password) {
+    return await createUserWithEmailAndPassword(this.auth, email, password);
+  }
+  async login(email, password) {
+    return await signInWithEmailAndPassword(auth, email, password);
+  }
+  async logout() {
+    return await signOut(this.auth);
+  }
 }
